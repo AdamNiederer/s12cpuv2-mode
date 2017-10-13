f41057b0485786845cd922137ef86244d1907e6e
Adam <adam.niederer@gmail.com>
2017-10-13 16:36:27 -0400

    Fix indentation on labels with non-word-constituents

--- s12cpuv2-mode.el
+++ s12cpuv2-mode.el
@@ -112,14 +112,23 @@
 (defconst s12cpuv2-empty-label-re
   (concat s12cpuv2-label-re " *$"))
 
+(defconst s12cpuv2-instruction-re
+  (regexp-opt s12cpuv2-instructions 'words))
+
+(defconst s12cpuv2-register-re
+  (regexp-opt s12cpuv2-instructions 'words))
+
+(defconst s12cpuv2-ops-re
+  (regexp-opt s12cpuv2-ops 'words))
+
 (defconst s12cpuv2-font-lock-defaults
   `((;; labels
      (,s12cpuv2-label-re . font-lock-function-name-face)
      ;; Instructions and operations
-     (,(regexp-opt s12cpuv2-instructions 'words) . font-lock-keyword-face)
-     (,(regexp-opt s12cpuv2-ops 'words) . font-lock-keyword-face)
+     (,s12cpuv2-instruction-re . font-lock-keyword-face)
+     (,s12cpuv2-ops-re . font-lock-keyword-face)
      ;; registers
-     (,(regexp-opt s12cpuv2-registers 'words) . font-lock-variable-name-face))))
+     (,s12cpuv2-register-re . font-lock-variable-name-face))))
 
 (defcustom s12cpuv2-tab-width 16
   "Width of a tab for S12CPUV2 mode."
@@ -164,7 +173,7 @@
     (save-excursion
       ;; Align instruction to `s12cpuv2-tab-width' characters
       (beginning-of-line)
-      (forward-word 2)
+      (re-search-forward s12cpuv2-instruction-re)
       (backward-word)
       (let ((instr-space-delta (- (point) (point-at-bol) 16)))
         (if (> instr-space-delta 0)
